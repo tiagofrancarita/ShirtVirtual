5133ae8fd0b4a3350845e75f86a0706a8927297f
Tiago França <tiagofranca.rita@gmail.com>
2023-02-01 22:31:24 -0300

    Criação das classes de modelo Parte IV.

---
+++ src/main/java/br/com/franca/ShirtVirtual/model/ContaPagar.java
@@ -0,0 +1,135 @@
+package br.com.franca.ShirtVirtual.model;
+
+import br.com.franca.ShirtVirtual.enums.StatusContaPagar;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_forn_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+    private Pessoa pessoaFornecedor;
+
+    @Column(name = "descricao_conta_receber", nullable = false)
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusContaPagar statusContaPagar;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_vencimento_conta_receber", nullable = false)
+    private Date dtVencimento;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_pagamento_conta_receber", nullable = false)
+    private Date dtPagamento;
+
+    @Column(name = "valor_total_conta_receber", nullable = false)
+    private BigDecimal valorTotal = BigDecimal.valueOf(0.00);
+
+    @Column(name = "valor_desconto_conta_receber", nullable = false)
+    private BigDecimal valorDesconto = BigDecimal.valueOf(0.00);
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Pessoa getPessoaFornecedor() {
+        return pessoaFornecedor;
+    }
+
+    public void setPessoaFornecedor(Pessoa pessoaFornecedor) {
+        this.pessoaFornecedor = pessoaFornecedor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusContaPagar getStatusContaPagar() {
+        return statusContaPagar;
+    }
+
+    public void setStatusContaPagar(StatusContaPagar statusContaPagar) {
+        this.statusContaPagar = statusContaPagar;
+    }
+
+    public Date getDtVencimento() {
+        return dtVencimento;
+    }
+
+    public void setDtVencimento(Date dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+
+    public Date getDtPagamento() {
+        return dtPagamento;
+    }
+
+    public void setDtPagamento(Date dtPagamento) {
+        this.dtPagamento = dtPagamento;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ContaPagar)) return false;
+        ContaPagar that = (ContaPagar) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
